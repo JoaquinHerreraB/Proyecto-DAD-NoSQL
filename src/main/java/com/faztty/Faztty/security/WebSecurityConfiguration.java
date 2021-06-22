699d83ea36c245ff34a8b8449cf8f29d70eea38d
JoaquinHerreraB <70816183+JoaquinHerreraB@users.noreply.github.com>
2021-06-22 02:36:17 -0500

    +Conexion-MongoDB-LOCAL

---
+++ src/main/java/com/faztty/Faztty/security/WebSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.faztty.Faztty.security;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+   /* @Override
+    public void configure(HttpSecurity http) throws Exception {
+       http.csrf().disable().authorizeRequests()
+        .antMatchers("/").permitAll()
+        .antMatchers(HttpMethod.POST,"/newuser").permitAll()
+        .antMatchers(HttpMethod.POST, "/login").permitAll()
+        .antMatchers(HttpMethod.POST,"/newuser/*").permitAll()
+        .antMatchers(HttpMethod.GET,"/master/*").permitAll()
+         .antMatchers(HttpMethod.GET,"/exploreCourse").permitAll()
+        .anyRequest().authenticated();
+    }*/
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.cors().and().csrf().disable();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
